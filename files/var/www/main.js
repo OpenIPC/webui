408e091d560fea8dc421db241b8b85639fb94e02
Paul Philippov <themactep@gmail.com>
2021-12-07 00:18:17 -0500

    switch autocomplete off for all forms.

--- files/var/www/main.js
+++ files/var/www/main.js
@@ -34,8 +34,8 @@ function engage() {
             }
         }
 
+        $$('form').forEach(el => el.autocomplete = 'off');
         $$('input[data-for]').forEach(el => el.addEventListener('click', ev => toggleAuto(ev.target)));
-        $$('select').forEach(el => el.autocomplete = 'off');
         $$('.btn-danger').forEach(el => el.addEventListener('click', ev => (!confirm("Are you sure?")) ? ev.preventDefault() : null));
         $$('a[href^=http]').forEach(el => el.target = '_blank');
     }

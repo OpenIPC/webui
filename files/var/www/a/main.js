147402e58cf9c8b54fce909c8b08e441449baa9c
Paul Philippov <themactep@gmail.com>
2022-06-24 00:11:50 -0400

    mostly formatting

--- files/var/www/a/main.js
+++ files/var/www/a/main.js
@@ -20,52 +20,67 @@ function refresh() {
                 el.dataset.value = el2.value;
                 el2.value = 'auto';
                 el2.disabled = true
+                $('#' + el2.id + '-value').textContent = 'auto';
             } else {
                 el2.value = el.dataset.value;
-                el2.disabled = false
+                el2.disabled = false;
+                $('#' + el2.id + '-value').textContent = el2.value;
             }
         }
 
         $$('form').forEach(el => el.autocomplete = 'off');
-        $$('input.auto-value').forEach(el => el.addEventListener('click', ev => toggleAuto(ev.target)));
-        $$('.btn-danger, .btn-warning, .confirm').forEach(el => el.addEventListener('click', ev => (!confirm("Are you sure?")) ? ev.preventDefault() : null));
+
+        $$('.btn-danger, .btn-warning, .confirm').forEach(el =>
+            el.addEventListener('click', ev =>
+                (!confirm("Are you sure?")) ? ev.preventDefault() : null));
+
         $$('.refresh').forEach(el => el.addEventListener('click', refresh));
+
         $$('a[href^=http]').forEach(el => el.target = '_blank');
-        $$('input.pat-host').forEach(el => el.pattern = '^[a-zA-Z0-9-_.]+$');
-        $$('input.pat-host-ip').forEach(el => el.pattern = '^[a-zA-Z0-9-_.]+$');
-        $$('input[type=range]').forEach(el => el.addEventListener('input', ev => $('#' + ev.target.id + '-value').textContent = ev.target.value));
 
-        const resizeObserver = new ResizeObserver(entries => {
-            entries.forEach(entry => {
-                if (entry.target.clientHeight > document.documentElement.clientHeight / 2) {
-                    entry.target.classList.add("log-scroll");
-                    entry.target.scrollTo(0, entry.target.scrollHeight);
-                }
-            });
+        $$('input.auto-value').forEach(el => {
+            el.addEventListener('click', ev => toggleAuto(ev.target));
+            toggleAuto(el);
         });
-        $$('pre').forEach(el => resizeObserver.observe(el));
 
-        $$(".password input[type=checkbox]").forEach(el => el.addEventListener('change', ev => $('#' + ev.target.dataset['for']).type = (el.checked) ? 'text' : 'password'));
+        // add patterns
+        $$('input.pat-host').forEach(el => el.pattern = '^[a-zA-Z0-9-_.]+$');
 
-        if ($("#send-to-telegram")) {
-            $("#send-to-telegram").addEventListener("click", event => {
-                event.preventDefault();
-                if (!confirm("Are you sure?")) return false;
-                const xhr = new XMLHttpRequest();
-                xhr.open("GET", "/cgi-bin/telegram-bot-send.cgi");
-                xhr.send();
-            });
-        }
+        $$('input.pat-host-ip').forEach(el => el.pattern = '^[a-zA-Z0-9-_.]+$');
 
-        if ($("#send-to-yadisk")) {
-            $("#send-to-yadisk").addEventListener("click", event => {
-                event.preventDefault();
-                if (!confirm("Are you sure?")) return false;
-                const xhr = new XMLHttpRequest();
-                xhr.open("GET", "/cgi-bin/yadisk-bot-send.cgi");
-                xhr.send();
-            });
-        }
+        $$('input[type=range]').forEach(el =>
+            el.addEventListener('input', ev =>
+                $('#' + ev.target.id + '-value').textContent = ev.target.value));
+
+        // const resizeObserver = new ResizeObserver(entries => {
+        //     entries.forEach(entry => {
+        //         if (entry.target.clientHeight > document.documentElement.clientHeight / 2) {
+        //             entry.target.classList.add("log-scroll");
+        //             entry.target.scrollTo(0, entry.target.scrollHeight);
+        //         }
+        //     });
+        // });
+        // $$('pre').forEach(el => resizeObserver.observe(el));
+
+        $$(".password input[type=checkbox]").forEach(el =>
+            el.addEventListener('change', ev =>
+                $('#' + ev.target.dataset['for']).type = (el.checked) ? 'text' : 'password'));
+
+        $("#send-to-telegram") && $("#send-to-telegram").addEventListener("click", event => {
+            event.preventDefault();
+            if (!confirm("Are you sure?")) return false;
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", "/cgi-bin/telegram-bot-send.cgi");
+            xhr.send();
+        });
+
+        $("#send-to-yadisk") && $("#send-to-yadisk").addEventListener("click", event => {
+            event.preventDefault();
+            if (!confirm("Are you sure?")) return false;
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", "/cgi-bin/yadisk-bot-send.cgi");
+            xhr.send();
+        });
 
         // Bootstrap constructor
         if ($('nav[role=tablist]')) {
@@ -83,7 +98,6 @@ function refresh() {
                     a.ariaSelected = a.classList.contains('active') ? 'true' : 'false';
                 }
             });
-
             $$('div[role=tabpanel]')[0].classList.add("show", "active");
             $$('div[role=tabpanel]').forEach(el => {
                 el.classList.add("tab-pane", "fade");

284ea66b9df06b360c172b22c4f03a70c52a06fa
Paul Philippov <themactep@gmail.com>
2022-07-24 16:50:16 -0400

    assign unique exit codes to different errors.

--- files/usr/sbin/send2email.sh
+++ files/usr/sbin/send2email.sh
@@ -14,9 +14,9 @@ fi
 
 # validate mandatory values
 [ -z "$email_smtp_server"  ] && echo -e "SMTP server not found in config" && exit 2
-[ -z "$email_smtp_port"    ] && echo -e "SMTP port not found in config" && exit 2
-[ -z "$email_from_address" ] && echo -e "Sender's address not found in config" && exit 2
-[ -z "$email_to_address"   ] && echo -e "Recipient's address not found in config" && exit 2
+[ -z "$email_smtp_port"    ] && echo -e "SMTP port not found in config" && exit 3
+[ -z "$email_from_address" ] && echo -e "Sender's address not found in config" && exit 4
+[ -z "$email_to_address"   ] && echo -e "Recipient's address not found in config" && exit 5
 
 # assign default values if not set
 [ -z "$email_from_name" ] && email_from_name="OpenIPC Camera"
@@ -29,7 +29,8 @@ snapshot="/tmp/${plugin}_snap.jpg"
 # get image from camera
 curl "http://127.0.0.1/image.jpg?t=$(date +"%s")" --output "$snapshot" --silent
 if [ $? -eq 0 ]; then
-  curl --url smtp://${email_smtp_server}:${email_smtp_port} --mail-from ${email_from_address} --mail-rcpt ${email_to_address} --user ${email_smtp_login}:${email_smtp_password} \
+  curl --url smtp://${email_smtp_server}:${email_smtp_port} --user ${email_smtp_login}:${email_smtp_password} \
+    --mail-from ${email_from_address} --mail-rcpt ${email_to_address} \
     -F '=(;type=multipart/mixed' -F "=${email_body};type=text/plain" -F "file=@${snapshot};type=image/jpeg;encoder=base64" -F '=)' \
     -H "Subject: ${email_subject}" -H "From: \"${email_from_name}\" <${email_from_address}>" -H "To: \"${email_to_name}\" <${email_to_address}>"
 else

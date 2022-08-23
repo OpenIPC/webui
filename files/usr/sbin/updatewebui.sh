c9484b60c0b1c51e4f817a8181f2767323ffb979
Paul Philippov <themactep@gmail.com>
2022-08-23 09:32:13 -0400

    refactor script.

--- files/usr/sbin/updatewebui.sh
+++ files/usr/sbin/updatewebui.sh
@@ -6,6 +6,17 @@ echo_c() {
   echo -e "$t"
 }
 
+log_and_run() {
+  echo_c 36 "$1"
+  eval "$1"
+}
+
+clean_quit() {
+  echo_c 37 "$2" >&2
+  [ -f "$tmp_file" ] && rm $v_opts $tmp_file
+  exit $1
+}
+
 print_usage() {
   echo "Usage: $0 -b <branch> [-v] [-f] [-h]
   -b <branch>  Git branch to use.
@@ -21,56 +32,43 @@ while getopts b:fvh flag; do
   case ${flag} in
   b) branch=${OPTARG} ;;
   f) enforce=1 ;;
-  v) verbose=1 ;;
+  v) verbose=1
+    v_opts="-v"
+    ;;
   h) print_usage ;;
   esac
 done
 
+# validation
 if [ -z "$branch" ]; then
   echo_c 33 "Mandatory parameter branch is not set!"
   print_usage
   exit 0
 fi
 
-tmp_file=/tmp/microbe-web.zip
 etag_file=/root/.ui.etag
+tmp_file="$(mktemp -u)"
+cmd="curl --silent --location --insecure --fail"
+[ "1" = "$verbose" ] && cmd="${cmd} --verbose"
+cmd="${cmd} --etag-save ${etag_file}"
+[ "1" != "$enforce" ] && [ -f "$etag_file" ] && cmd="${cmd} --etag-compare ${etag_file}"
+cmd="${cmd} --url https://github.com/OpenIPC/microbe-web/archive/refs/heads/${branch}.zip"
+cmd="${cmd} --output $tmp_file"
+log_and_run "$cmd"
+[ ! -f "$tmp_file" ] && clean_quit 1 "GitHub version matches the installed one. Nothing to update."
 
-# url to retrieve update from
-url="https://github.com/OpenIPC/microbe-web/archive/refs/heads/${branch}.zip"
-curl_opts="--location --insecure --etag-save ${etag_file}" #--silent
-
-if [ "1" != "$enforce" ]; then
-  [ -f "$etag_file" ] && curl_opts="${curl_opts} --etag-compare ${etag_file}"
-fi
-
-if [ "1" = "$verbose" ]; then
-  curl_opts="${curl_opts} --verbose"
-  unzip_opts=""
-  cp_opts="-v"
-  rm_opts="-v"
-else
-  curl_opts="${curl_opts} --silent"
-  unzip_opts="-q"
-  cp_opts=""
-  rm_opts=""
-fi
-
-cmd="curl $curl_opts --output $tmp_file --url $url"
-echo_c 36 "$cmd"
-$cmd
-
-if [ ! -f "$tmp_file" ]; then
-  echo_c 37 "GitHub version matches the installed one. Nothing to update." >&2
-  exit 1
-fi
-
-commit=$(tail -c 40 $tmp_file|cut -b1-7)
-_ts=$(unzip -l $tmp_file|head -5|tail -1|xargs|cut -d" " -f2)
-timestamp="$(echo $_ts|cut -d- -f3)-$(echo $_ts|cut -d- -f1)-$(echo $_ts|cut -d- -f2)" # ugly but it works
+commit=$(tail -c 40 $tmp_file | cut -b1-7)
+# date in ISO format. ugly but it works
+_ts=$(unzip -l $tmp_file | head -5 | tail -1 | xargs | cut -d" " -f2)
+timestamp="$(echo $_ts | cut -d- -f3)-$(echo $_ts | cut -d- -f1)-$(echo $_ts | cut -d- -f2)"
 
 unzip_dir="/tmp/microbe-web-${branch}"
 [ -d "$unzip_dir" ] && rm -rf $unzip_dir
-unzip $unzip_opts -o -d /tmp $tmp_file -x microbe-web-dev/README.md microbe-web-dev/.git* microbe-web-dev/LICENSE microbe-web-dev/docs/* microbe-web-dev/wirebox/*
+
+cmd="unzip -o -d /tmp $tmp_file"
+[ "1" = "$verbose" ] && cmd="${cmd} -q"
+cmd="${cmd} -x microbe-web-dev/README.md microbe-web-dev/LICENSE microbe-web-dev/.git* microbe-web-dev/dev/* microbe-web-dev/docs/*"
+log_and_run "$cmd"
 upd_dir="${unzip_dir}/files"
 
 echo_c 37 "Copy newer files to web directory"
@@ -79,29 +77,27 @@ for upd_file in $(find $upd_dir -type f -or -type l); do
   # echo "Overlay file ${ovl_file}"
   if [ ! -f "$ovl_file" ] || [ "$(diff -q $ovl_file $upd_file)" ]; then
     [ ! -d "${ovl_file%/*}" ] && mkdir -p $(dirname $ovl_file)
-    cp $cp_opts -f $upd_file $ovl_file
+    cp $v_opts -f $upd_file $ovl_file
   fi
 done
 
 echo_c 37 "Remove absent files from overlay"
 for file in $(diff -qr "/var/www" "${upd_dir}/var/www" | grep "Only in /var/www:" | cut -d':' -f2 | tr -d "^ "); do
-  [ "$file" != "$etag_file" ] && rm $rm_opts -f /var/www/${file}
+  [ "$file" != "$etag_file" ] && rm $v_opts -f /var/www/${file}
   mount -o remount /
 done
 
 echo_c 37 "Delete bundle"
-rm $rm_opts -f $tmp_file
+rm $v_opts -f $tmp_file
 
 echo_c 37 "Delete temp directory"
-rm $rm_opts -f -r /tmp/microbe-web-${branch}
+rm $v_opts -rf /tmp/microbe-web-${branch}
 
-if [ -z "$error" ]; then
-  echo "${branch}+${commit}, ${timestamp}" >/var/www/.version
-  [ -f /tmp/sysinfo.txt ] && rm $rm_opts /tmp/sysinfo.txt
-  echo_c 37 "Done."
-  exit 0
-else
-  rm $rm_opts $etag_file
-  echo_c 31 "ATTENTION! There were errors!" >&2
-  exit 2
+if [ -n "$error" ]; then
+  rm $v_opts $etag_file
+  clean_quit 2 "ATTENTION! There were errors!"
 fi
+
+echo "${branch}+${commit}, ${timestamp}" >/var/www/.version
+[ -f /tmp/sysinfo.txt ] && rm $v_opts /tmp/sysinfo.txt
+clean_quit 0 "Done."
